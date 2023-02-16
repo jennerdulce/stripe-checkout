c0adc92ed39fe2a8338effc48cee6e212cf5f1f5
Jenner Dulce <61197981+jennerdulce@users.noreply.github.com>
2023-02-16 14:20:33 -0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,3 +1,70 @@
+
+import React, { useEffect, useState } from "react"
+import {
+  PaymentElement,
+  useStripe,
+  useElements
+} from "@stripe/react-stripe-js"
+import { useDispatch, useSelector } from 'react-redux'
+import { paymentSucessStatus } from 'ControlActions/CourseListAction'
+import InputBase from '@material-ui/core/InputBase'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
+import {
+  alpha,
+  withStyles,
+  makeStyles,
+} from '@material-ui/core/styles'
+import { useForm } from 'react-final-form'
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.common.white,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    width: 'auto',
+    padding: '10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}))(InputBase)
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  inputLabel: {
+    fontSize: '18px'
+  }
+}))
+
 export default function CheckoutForm() {
   const dispatch = useDispatch()
   const classes = useStyles()
